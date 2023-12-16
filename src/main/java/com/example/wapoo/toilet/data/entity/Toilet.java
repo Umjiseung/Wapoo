8f2aaf7164122bce09d17c784711b680baef02c5
Umjiseung <eom071005123@gmail.com>
2023-12-16 22:45:00 +0900

    update::table 이름 변경

--- src/main/java/com/example/wapoo/toilet/data/entity/Toilet.java
+++ src/main/java/com/example/wapoo/toilet/data/entity/Toilet.java
@@ -9,27 +9,27 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
-@Table(name = "wapoo")
+@Table(name = "toilet")
 public class Toilet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "wapoo_id")
+    @Column(name = "toilet_id")
     private Long id;
 
-    @Column(name = "wapoo_state", nullable = false)
+    @Column(name = "toilet_state", nullable = false)
     private Integer state;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "wapoo_location",nullable = false)
+    @Column(name = "toilet_location",nullable = false)
     private Location location;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "wapoo_floor",nullable = false)
+    @Column(name = "toilet_floor",nullable = false)
     private Floor floor;
 
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "wapoo_gender", nullable = false)
+    @Column(name = "toilet_gender", nullable = false)
     private Gender gender;
 
     public void update(Integer state) {
