d26cd654c35a5b4587a16fb9d9a4aebfaf07d409
Umjiseung <eom071005123@gmail.com>
2023-12-18 14:07:03 +0900

    delete::안 쓰는 import문 정리

--- src/main/java/com/example/wapoo/toilet/data/dto/request/LocationUpdateRequest.java
+++ src/main/java/com/example/wapoo/toilet/data/dto/request/LocationUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.example.wapoo.toilet.data.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
