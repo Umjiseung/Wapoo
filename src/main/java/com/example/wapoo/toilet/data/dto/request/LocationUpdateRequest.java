5acc5481820b7b331cca4b62fd71156a4189c478
Umjiseung <eom071005123@gmail.com>
2023-12-18 11:04:02 +0900

    update::유효성 검사 어노테이션 변경

--- src/main/java/com/example/wapoo/toilet/data/dto/request/LocationUpdateRequest.java
+++ src/main/java/com/example/wapoo/toilet/data/dto/request/LocationUpdateRequest.java
@@ -1,15 +1,16 @@
 package com.example.wapoo.toilet.data.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class LocationUpdateRequest {
-    @NotBlank
+    @NotNull
     private Long id;
-    @NotBlank
+    @NotNull
     private Integer state;
 
 }
